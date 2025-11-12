856dc617410ab1edf59c61db428a0166b79ca134
Beyza ASLAN <aslanbeyza3413@gmail.com>
2025-11-13 01:34:13 +0300

    category id  page route completed

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="card" style="width: 18rem;">
+        <h1>Kategori Detayları</h1>
+        <a type="button" class="btn btn-dark" href={{route('categories.index')}}>Back</a>
+        <img src="https://cdn-icons-png.flaticon.com/512/219/219983.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ $category->name }}</h5>
+            <p class="card-text">{{ $category->color }}</p>
+            <p class="card-text">{{ $category->description }}</p>
+            <button type="button" class="btn btn-danger">Edit</button>
+            <button type="button" class="btn btn-secondary">Delete</button>
+            </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
